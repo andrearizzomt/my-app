972a85cf3292ca35eda5999edfeb6a0e3a35ad3f
Andrea Rizzo <andrearizzo85@gmail.com>
2022-04-19 15:44:59 +0200

    React my-app tutorial testing GIT Push

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// import ReactDOM from "react-dom";
+// import React from "react";
+// import "./styles.css";
+
+// for images
+// import imageName from "../images/imagename.png"
+// replace <img> src with imageName in curlybraces like so src={imageName}
+
+// function Page() {
+//   return (
+//     <div>
+//       <ol>
+//         <li>Item 1</li>
+//         <li>Item 2</li>
+//         <li>Item 3</li>
+//         <li>Item 4</li>
+//         <li>Item 5</li>
+//       </ol>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<Page />, document.getElementById("root"));
+
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+import "./styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
